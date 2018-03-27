adbe09f68af2048cd2e473a15e1e392765fe2bdf
himanshiiiii <himanshi.gupta@tothenew.com>
2018-03-27 16:55:56 +0530

    final submission

--- src/main/groovy/Question23.groovy
+++ src/main/groovy/Question23.groovy
@@ -1,7 +1,7 @@
 class Question23 {
 
     static void main(String[] args) {
-
+git
         String string = "http://www.google.com?name=johny&age=20&hobby=cricket"
         URL url = new URL(string)
         String query = url.getQuery()
